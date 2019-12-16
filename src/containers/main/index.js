d921e1f8dd53d1fde53dae024bafb9b5db6e1849
EmmanuelYupit <emmanuelyupit08@gmail.com>
2019-12-16 16:43:05 -0500

    modal component with repo description

--- src/containers/main/index.js
+++ src/containers/main/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Text } from 'react-native';
 import { Wrap } from './index.style';
+import RepoDetail from '../../components/RepoDetail';
 
 const App = () => (
     <Wrap>
-        <Text>Hello, dude!</Text>
+        <RepoDetail />
     </Wrap>
 );
 

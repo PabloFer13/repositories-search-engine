e79c726e78d53af6349748c78ef5d61f730c52ed
Pablo Fer <pablofer.melendez@gmail.com>
2019-12-17 12:37:49 -0500

    Update reducers.js

--- src/containers/reducers.js
+++ src/containers/reducers.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import app from './Home/reducer';
 
-export default (createRootReducer = () =>
+export default createRootReducer = () =>
     combineReducers({
         app,
-    }));
+    });
